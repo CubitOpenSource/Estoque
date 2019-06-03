42adf6c42378ff05b2b9b90307b7b26b4fc98dc5
ivanfilho21 <ivanfilho21@gmail.com>
2019-06-03 06:00:32 -0300

    Added product-form page

--- CubitOpenSource/Estoque/mvc/controllers/ProductController.php
+++ CubitOpenSource/Estoque/mvc/controllers/ProductController.php
@@ -15,6 +15,7 @@ class ProductController extends Controller
 
 	public function new()
 	{
-
+		$data = array();
+		$this->loadView("product-form", $data);
 	}
 }
--- CubitOpenSource/Estoque/mvc/views/product-form.php
+++ CubitOpenSource/Estoque/mvc/views/product-form.php
@@ -1,12 +1,56 @@
-<?php $title = "New Product"; ?>
-<?php include "../template/page-top.php"; ?>
-
 <form name="product" method="POST">
 	<input type="hidden" name="id">
 
-	<input type="number" name="code">
+	<label>Cógigo do Produto:</label>
+	<input type="number" name="code" min="1" autofocus="on">
 
-	<input type="text" name="name" placeholder="Product Name:">
+	<label>Código de Barras:</label>
+	<input type="text" name="barcode">
+
+	<label>Nome:</label>
+	<input type="text" name="name">
+
+	<label>Descrição:</label>
+	<input type="text" name="description">
+
+	<label>Unidade</label>
+	<select name="unity">
+		<option value="1">UN</option>
+	</select>
+
+	<fieldset>
+		<legend>Financeiro</legend>
+
+		<label>Preço de Compra</label>
+		<input type="text" name="price-a">
+
+		<label>Preço de Venda</label>
+		<input type="text" name="price-b">
+
+		<label>Margem de Lucro</label>
+		<input type="text" name="gain-percent">
+	</fieldset>
+
+	<fieldset>
+		<legend>Estoque</legend>
+
+		<label>Qtd em Estoque</label>
+		<input type="number" name="stock" min="0">
+
+		<label>Estoque Mínimo</label>
+		<input type="number" name="min-stock" min="0">
+
+		<label>Localização</label>
+		<input type="text" name="location">
+	</fieldset>
+
+	<fieldset>
+		<legend>Outras Opções</legend>
+
+		<label>Produto está Ativo:</label>
+		<select name="active">
+			<option value="1">Sim</option>
+			<option value="2">Não</option>
+		</select>
+	</fieldset>
 </form>
-
-<?php include "./CubitOpenSource/Estoque/template/page-bottom.php"; ?>
